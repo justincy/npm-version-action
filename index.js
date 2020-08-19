f0f676a24fcafd4da337a2156951ba1fc8cf3b1a
Justin York <jyork@genward.com>
2020-08-18 19:07:41 -0600

    Exit code

--- index.js
+++ index.js
@@ -3,4 +3,7 @@ const {version} = require(resolve('./package.json'));
 
 if (version) {
   console.log(`::set-output name=version::${version}`);
+} else {
+  console.error('no version found');
+  process.exit(1);
 }
