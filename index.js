2a0655432f40bb7589c0b41426acc785b835456b
Justin York <jyork@genward.com>
2020-08-18 18:59:14 -0600

    other debug

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
+const {execSync} = require('child_process');
 const {version} = require('./package.json');
+
+console.log(execSync('pwd'));
+console.log(execSync('ls -al'));
 
 if (version) {
   console.log(`::set-output name=version::${version}`);
